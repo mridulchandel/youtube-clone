686ac8540b106b3d5cf7422324690c0a61f1ba4b
mridulchandel <43166857+mridulchandel@users.noreply.github.com>
2020-09-02 12:48:50 +0530

    react-headroom for sticky header

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Headroom from "react-headroom";
 
 import YoutubeLogo from "../common/YoutubeLogo";
 import SearchBar from "../common/SearchBar";
@@ -13,26 +14,28 @@ const Header = ({
   handleSearchClick,
 }) => {
   return (
-    <Container
-      fluid
-      className="header-component d-flex py-4 justify-content-between"
-    >
-      <div className="header-subComponent d-flex justify-content-between">
-        <FontAwesomeIcon
-          icon={faBars}
-          size="2x"
-          onClick={showSideBar}
-          className="header-component-barIcon"
+    <Headroom>
+      <Container
+        fluid
+        className="header-component d-flex py-4 justify-content-between"
+      >
+        <div className="header-subComponent d-flex justify-content-between">
+          <FontAwesomeIcon
+            icon={faBars}
+            size="2x"
+            onClick={showSideBar}
+            className="header-component-barIcon"
+          />
+          <YoutubeLogo size="2x" />
+        </div>
+        <SearchBar
+          searchStyle={{ width: "50vw", maxWidth: "30rem" }}
+          searchValue={searchValue}
+          handleSearchValue={handleSearchValue}
+          handleSearchClick={handleSearchClick}
         />
-        <YoutubeLogo size="2x" />
-      </div>
-      <SearchBar
-        searchStyle={{ width: "50vw", maxWidth: "30rem" }}
-        searchValue={searchValue}
-        handleSearchValue={handleSearchValue}
-        handleSearchClick={handleSearchClick}
-      />
-    </Container>
+      </Container>
+    </Headroom>
   );
 };
 
